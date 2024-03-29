f3084f166a8517b87cdac477b076870a20c9dbb7
vaibhavdachawar <123737345+vaibhavdachawar@users.noreply.github.com>
2024-03-29 07:35:49 +0530

    Do

--- src/test/java/runner/Campaign_tag_RunnerClass.java
+++ src/test/java/runner/Campaign_tag_RunnerClass.java
@@ -8,8 +8,8 @@ import io.cucumber.testng.CucumberOptions;
 		
 		features= {"src\\test\\resources\\application\\Campaign_tags.feature"},
 		glue= {"loginstep","hooks"},
-		plugin= {"pretty"},
-// "html:target/cucumber-reports/Campaign_tag.html","json:target/cucumber-report.json",
+		plugin= {"pretty","html:target/cucumber-reports/Campaign_tag.html","json:target/cucumber-report.json"},
+// 
 //		tags="@Regression"
 		publish=true
 )

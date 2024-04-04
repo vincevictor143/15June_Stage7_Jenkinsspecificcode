2a4c183cfec2e691b623c82703ab0d732fae47e9
vaibhavdachawar <123737345+vaibhavdachawar@users.noreply.github.com>
2024-04-04 08:01:57 +0530

    do

--- src/test/java/runner/Campaign_tag_RunnerClass.java
+++ src/test/java/runner/Campaign_tag_RunnerClass.java
@@ -9,8 +9,9 @@ import io.cucumber.testng.CucumberOptions;
 		features= {"src\\test\\resources\\application\\Campaign_tags.feature"},
 		glue= {"loginstep","hooks"},
 		plugin= {"pretty","html:target/cucumber-reports/Campaign_tag.html","json:target/cucumber-report.json"},
+		dryRun=true,
 // 
-//		tags="@Regression"
+
 		publish=true
 )
 public class Campaign_tag_RunnerClass extends AbstractTestNGCucumberTests
